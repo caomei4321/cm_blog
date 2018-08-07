ec5a6805d86af131f083f276b924ae8a2beb5215
cm <43211374@qq.com>
2018-08-07 17:47:23 +0800

    添加访客记录

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,10 +5,12 @@ namespace App\Http\Controllers;
 use App\Http\Requests;
 use Illuminate\Http\Request;
 use App\Repositories\ArticleRepositoryEloquent;
+use App\Models\Visitor;
 
 class HomeController extends Controller
 {
     protected $article;
+
 
     public function __construct(ArticleRepositoryEloquent $article)
     {
@@ -20,8 +22,11 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
+        $server = $_SERVER;
+        $vistors = new Visitor;
+
         $articles = $this->article
             ->with([
                 'category'
@@ -29,6 +34,30 @@ class HomeController extends Controller
             ->orderBy('sort','desc')
             ->orderBy('id', 'desc')
             ->paginate();
+
+        if(!array_key_exists('HTTP_REFERER',$server)){
+
+            $date = date('Y-m-d',time());
+
+            $address = $request->getClientIp(); //获取客户端地址
+
+            $vistor = $vistors->query()->where('created_at','like',$date.'%')->where('address',$address)->first();
+
+            if ($vistor) {
+
+                /*dd($vistor->getAttribute('count'));*/
+                $vistor->count = $vistor->count + 1;
+
+                $vistor->save();
+                return view('default.home', compact('articles'));
+            }
+
+
+            $vistors->address = $address;
+            $vistors->save();
+        }
+
         return view('default.home', compact('articles'));
+
     }
 }
---
+++ database/migrations/2018_08_07_075933_create_visitors_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVisitorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('visitors', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('address')->default('');
+            $table->integer('count')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('models_visitors');
+    }
+}
