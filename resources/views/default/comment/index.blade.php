6379b3be11455bad15b070079de5dfdd5cd009e7
cm <43211374@qq.com>
2018-08-29 18:16:29 +0800

    关闭畅言“

--- resources/views/default/comment/index.blade.php
+++ resources/views/default/comment/index.blade.php
@@ -3,12 +3,11 @@
 $commentPlugin = $systemPresenter->getKeyValue('comment_plugin');
 $shortName = $systemPresenter->getKeyValue($commentPlugin.'_short_name');
 ?>
-@include('default.comment.changyan')
-{{--
+{{--@include('default.comment.changyan')--}}
 @if($commentPlugin !='' && $shortName != '')
     @if($commentPlugin == 'duoshuo')
         @include('default.comment.duoshuo')
     @elseif($commentPlugin == 'disqus')
         @include('default.comment.disqus')
     @endif
-@endif--}}
+@endif
